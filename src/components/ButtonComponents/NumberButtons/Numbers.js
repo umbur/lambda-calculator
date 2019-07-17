ffec1c018fb420006e171ee76b56ce03ca6869bb
umbur <you@example.com>
2019-07-16 18:09:04 -0600

    passed data from Numbers to the child component NumbersButton

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 //import any components needed
-import {numbers} from '../../../data.js'
+import { numbers } from '../../../data.js'
+import NumberButton from './NumberButton.js'
 //Import your array data to from the provided data file
 
 const Numbers = () => {
@@ -11,6 +12,7 @@ const Numbers = () => {
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {numbers.map( (number) => <NumberButton key={index} number={number} />)}
     </div>
   );
 };
