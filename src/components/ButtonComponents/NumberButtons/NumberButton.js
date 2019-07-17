0681de7c1e9efa49cb2aad65145fd1e7fc30fff6
umbur <you@example.com>
2019-07-16 18:21:16 -0600

    displays on the screen: numbers, specials, operators

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,11 +1,12 @@
+    
 import React from "react";
 
 const NumberButton = (props) => {
-  //console.log('numberbutton props:', props)
-  return (
-    <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
-      <button>{props.number}</button>
-    </>
-  );
+ return (
+   <>
+     {/* Display a button element rendering the data being passed down from the parent container on props */}
+     <button>{props.number}</button>
+   </>
+ );
 };
+export default NumberButton
